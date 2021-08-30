8454a71610e7bd7a9451304a52842fa70fc7eaed
puneet2508 <puneetdhiman56@gmail.com>
2021-08-31 00:53:56 +0530

    added user auth

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
-// const serverless = require('serverless-http');
-const path = require('path');
 const cors = require('cors');
+const path = require('path');
+const bcrypt = require('bcryptjs');
 
 const Datastore = require('nedb');
 
 const db = new Datastore('./database.db');
 db.loadDatabase();
+const users = new Datastore('./users.db');
+users.loadDatabase();
 
 const app = express();
 const port = 3001;
@@ -14,9 +16,9 @@ app.use(cors());
 
 app.use(express.json());
 
-app.get('/todos', (req, res) => {
-	console.log('got a fetch req');
-	db.find({}, (error, data) => {
+app.post('/todos/gettodos', (req, res) => {
+	console.log('App started, all tasks sent');
+	db.find({ email: req.body.email }, (error, data) => {
 		if (error) {
 			console.log(error);
 			return;
@@ -25,13 +27,14 @@ app.get('/todos', (req, res) => {
 	});
 });
 
-app.post('/todos', (req, res) => {
+app.post('/todos/settodos', (req, res) => {
+	console.log('Task added-', req.body.title);
 	db.insert(req.body);
 	res.end();
 });
 
 app.post('/todos/complete', (req, res) => {
-	console.log('got a request');
+	console.log('Task completed-', req.body.title);
 	db.update(
 		{ id: req.body.id },
 		{ $set: { completed: true } },
@@ -44,14 +47,74 @@ app.post('/todos/complete', (req, res) => {
 	res.end();
 });
 app.post('/todos/delete', (req, res) => {
-	console.log('got a request');
+	console.log('Task deleted-', req.body.id);
 	db.remove({ id: req.body.id }, {}, function (err, numRemoved) {
 		console.log(numRemoved);
 	});
 	res.end();
 });
 
+app.post('/todos/login', (req, res) => {
+	console.log('login requested');
+	users.find({ email: req.body.email }, (error, user) => {
+		if (error) {
+			console.log('Error happened', error);
+			res.json({ status: 'Server error.Try after some time' });
+		} else if (user.length >= 1) {
+			console.log('user found');
+			console.log(user[0]);
+			const passwordCheck = bcrypt.compareSync(
+				req.body.password,
+				user[0].password
+			);
+			if (passwordCheck) {
+				console.log('password ok');
+				res.json({
+					status: 'Log in Sucessfull',
+					username: user[0].username,
+					email: user[0].email,
+				});
+			} else {
+				console.log('password not ok');
+				res.json({ status: 'Wrong Password' });
+			}
+		} else {
+			console.log('user not found');
+			res.json({ status: 'User Does not exist, Please SignUp' });
+		}
+	});
+});
+
+app.post('/todos/signup', async (req, res) => {
+	console.log(req.body);
+	const password = await bcrypt.hash(req.body.password, 12);
+
+	users.find({ email: req.body.email }, (error, user) => {
+		if (error) {
+			console.log(error);
+			res.error();
+		} else if (user.length >= 1) {
+			console.log(user);
+			res.json({ status: 'Email Already Axists' });
+		} else {
+			console.log('OK', user);
+			const data = {
+				username: req.body.username,
+				email: req.body.email,
+				password: password,
+			};
+			users.insert(data);
+			res.json({
+				status: 'Signup Sucessfull',
+				username: data.username,
+				email: data.email,
+			});
+		}
+	});
+});
+
 db.persistence.compactDatafile();
+users.persistence.compactDatafile();
 
 app.listen(process.env.PORT || port, () => {
 	console.log(`Listening.....`);
